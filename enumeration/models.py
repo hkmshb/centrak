816459213573ca3072f1f0f9dbc4df553235facd
hkmshb <hkmshb@gmail.com>
2015-08-17 14:15:41 +0100

    Added models Manufacturer and MobileOS

--- enumeration/models.py
+++ enumeration/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    
+    class Meta:
+        db_table = 'enum_manufacturer'
+    
+    def __str__(self):
+        return self.name
+
+
+class MobileOS(models.Model):
+    name     = models.CharField(max_length=25, unique=True)
+    provider = models.CharField(max_length=50, blank=True)
+    
+    class Meta:
+        db_table = 'enum_mobileos'
+    
+    def __str__(self):
+        return '%s (by %s)' % (self.name, self.provider)
+
+    
--- enumeration/tests.py
+++ enumeration/tests.py
@@ -1,3 +1,25 @@
+from django.core.exceptions import ImproperlyConfigured, ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from enumeration.models import Manufacturer, MobileOS
+
+
+
+class ManufacturerTest(TestCase):
+    
+    def test_duplicate_manufacturer_are_invalid(self):
+        Manufacturer.objects.create(name='Samsung')
+        with self.assertRaises(ValidationError):
+            manufacturer = Manufacturer(name='Samsung')
+            manufacturer.full_clean()
+
+
+class MobileOSTest(TestCase):
+    
+    def test_duplicate_mobileos_are_invalid(self):
+        MobileOS.objects.create(name='Android', provider='Google')
+        with self.assertRaises(ValidationError):
+            os = MobileOS(name='Android')
+            os.full_clean()
+    
+    
