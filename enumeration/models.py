3cb208ca9b36ff09c5a838945fcb967d87887877
hkmshb <hkmshb@gmail.com>
2015-08-17 19:55:06 +0100

    Added models Device and DeviceIMEI

--- enumeration/models.py
+++ enumeration/models.py
@@ -14,12 +14,43 @@ class Manufacturer(models.Model):
 
 class MobileOS(models.Model):
     name     = models.CharField(max_length=25, unique=True)
-    provider = models.CharField(max_length=50, blank=True)
+    provider = models.CharField(max_length=50, blank=True, default='')
     
     class Meta:
         db_table = 'enum_mobileos'
     
     def __str__(self):
-        return '%s (by %s)' % (self.name, self.provider)
+        return "%s (by %s)" % (self.name, self.provider)
 
+
+class Device(models.Model):
+    PHONE   = 'P'
+    TABLET  = 'T'
+    FORM_FACTOR_CHOICES = (
+        (PHONE,  'Phone'),
+        (TABLET, 'Tablet')
+    )
     
+    brand = models.ForeignKey(Manufacturer)
+    model = models.CharField(max_length=50)
+    mobile_os   = models.ForeignKey(MobileOS)
+    os_version  = models.CharField(max_length=25)
+    form_factor = models.CharField(max_length=1, 
+                    choices=FORM_FACTOR_CHOICES,
+                    default=PHONE)
+    serialno = models.CharField(max_length=25, blank=True)
+    
+    class Meta:
+        db_table = 'enum_device'
+    
+    
+class DeviceIMEI(models.Model):
+    device = models.ForeignKey(Device)
+    imei = models.CharField(max_length=25, unique=True)
+    
+    class Meta:
+        db_table = 'enum_device_imei'
+    
+    def __str__(self):
+        return "imei=%s" % self.imei
+
--- enumeration/tests.py
+++ enumeration/tests.py
@@ -1,7 +1,7 @@
-from django.core.exceptions import ImproperlyConfigured, ValidationError
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from enumeration.models import Manufacturer, MobileOS
+from enumeration.models import Manufacturer, MobileOS, Device, DeviceIMEI
 
 
 
@@ -22,4 +22,79 @@ class MobileOSTest(TestCase):
             os = MobileOS(name='Android')
             os.full_clean()
     
+
+class DeviceTest(TestCase):
     
+    def test_cannot_save_without_brand(self):
+        device = build_device_without_brand()
+        with self.assertRaises(ValidationError):
+            device.full_clean()
+     
+    def test_cannot_save_without_mobile_os(self):
+        device = build_device_without_mobile_os()
+        with self.assertRaises(ValidationError):
+            device.full_clean()
+    
+    def test_blank_serialno_allowed(self):
+        device = build_device()
+        device.serial_no = ''
+        device.full_clean()        
+
+
+class DeviceIMEITest(TestCase):
+    
+    def test_duplicate_imei_are_invalid(self):
+        device = build_device()
+        device.save()
+        
+        DeviceIMEI.objects.create(device=device, imei='35010203040506070809')
+        with self.assertRaises(ValidationError):
+            imei = DeviceIMEI(device=device, imei='35010203040506070809')
+            imei.full_clean()
+    
+    def test_blank_imei_not_allowed(self):
+        device = build_device()
+        device.save()
+        
+        with self.assertRaises(ValidationError):
+            imei = DeviceIMEI(device=device)
+            imei.full_clean()
+    
+    def test_returns_meaningful_string_repr(self):
+        imei = DeviceIMEI(build_device(), imei='35010203040506070809')
+        self.assertEqual('imei=35010203040506070809', str(imei))
+        
+
+def build_device():
+    brand = Manufacturer.objects.create(name='Samsung')
+    mobile_os = MobileOS.objects.create(name='Android')
+    return Device(
+        brand = brand,
+        model = 'Device Model',
+        mobile_os = mobile_os,
+        os_version = '1.0.0 (whatever)',
+        form_factor = Device.PHONE,
+        serialno = ''
+    )
+
+
+def build_device_without_brand():
+    mobile_os = MobileOS.objects.create(name='Android')
+    return Device(
+        model = 'Device Model',
+        mobile_os = mobile_os,
+        os_version = '1.0.0 (whatever)',
+        form_factor = Device.PHONE,
+        serialno = ''
+    )
+
+
+def build_device_without_mobile_os():
+    brand = Manufacturer.objects.create(name='Samsung')
+    return Device(
+        brand = brand,
+        model = 'Device Model',
+        os_version = '1.0.0 (whatever)',
+        form_factor = Device.PHONE,
+        serialno = ''
+    )    
