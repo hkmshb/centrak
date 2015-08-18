ac65a5f46b724e25304e9b40c545039cd950baf7
hkmshb <hkmshb@gmail.com>
2015-08-18 10:12:30 +0100

    Added django app 'main' with basic layout & styling

--- centrak/urls.py
+++ centrak/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
+    url(r'', include('main.urls')),
     url(r'^enum/', include('enumeration.urls')),
 )
---
+++ main/__init__.py
@@ -0,0 +1,3 @@
+"""
+django-app: main
+"""
